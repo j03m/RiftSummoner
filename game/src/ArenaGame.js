defa1f2b70f044cc48704d0c1fb1e5ab22891144
Joe Mordetsky <jmordetsky@zynga.com>
2013-11-22 15:59:58 -0500

    woops.

--- game/src/ArenaGame.js
+++ game/src/ArenaGame.js
@@ -179,6 +179,7 @@ var ArenaGame = jc.WorldLayer.extend({
 //        }.bind(this);
 //
 //        this.powerLayer.start();
+        callback();
 
     },
     presentTeam:function(team, formation, point, callback){
