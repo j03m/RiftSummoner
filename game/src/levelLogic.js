ab50b0d988620b161167be976a2b377b1feb4183
Joe Mordetsky <jmordetsky@zynga.com>
2013-12-03 17:04:57 -0500

    Forgot to update level logic team.

--- game/src/levelLogic.js
+++ game/src/levelLogic.js
@@ -1,40 +1,96 @@
 var hotr = hotr || {};
 hotr.levelLogic = {};
 
+//var temp =  [
+//    {
+//        "name":"ogre",
+//        "id":"id1",
+//        "data":{}
+//    },
+//    {
+//        "name":"ogre",
+//        "id":"id2",
+//        "data":{}
+//    },
+//    {
+//        "name":"ogre",
+//        "id":"id3",
+//        "data":{}
+//    },
+//    {
+//        "name":"ogre",
+//        "id":"id4",
+//        "data":{}
+//    },
+//    {
+//        "name":"orc",
+//        "id":"id5",
+//        "data":{}
+//    },
+//    {
+//        "name":"goblin",
+//        "data":{}
+//    },
+//    {
+//        "name":"goblin",
+//        "data":{}
+//    },
+//    {
+//        "name":"orc",
+//        "data":{}
+//    },
+//    {
+//        "name":"wizard",
+//        "data":{}
+//    },
+//    {
+//        "name":"wizard",
+//        "data":{}
+//    },
+//    {
+//        "name":"wizard",
+//        "data":{}
+//    },
+//    {
+//        "name":"wizard",
+//        "data":{}
+//    }
+//]
+
 var temp =  [
     {
         "name":"ogre",
         "id":"id1",
         "data":{}
     },
-    {
-        "name":"ogre",
-        "id":"id2",
-        "data":{}
-    },
-    {
-        "name":"ogre",
-        "id":"id3",
-        "data":{}
-    },
-    {
-        "name":"ogre",
-        "id":"id4",
-        "data":{}
-    },
-    {
-        "name":"orc",
-        "id":"id5",
-        "data":{}
-    },
-    {
-        "name":"goblin",
-        "data":{}
-    },
-    {
-        "name":"goblin",
-        "data":{}
-    },
+//    {
+//        "name":"ogre",
+//        "id":"id2",
+//        "data":{}
+//    },
+//    {
+//        "name":"ogre",
+//        "id":"id3",
+//        "data":{}
+//    },
+//    {
+//        "name":"ogre",
+//        "id":"id4",
+//        "data":{}
+//    },
+//    {
+//        "name":"orc",
+//        "id":"id5",
+//        "data":{}
+//    },
+//    {
+//        "name":"goblin",
+//        "data":{}
+//    },
+//    {
+//        "name":"goblin",
+//        "data":{}
+//    },
     {
         "name":"orc",
         "data":{}
@@ -56,8 +112,6 @@ var temp =  [
         "data":{}
     }
 ]
-
-
 
 hotr.levelLogic.getTeamForLevel = function(level){
     return temp;
