0957eec9e78c2d68e2e58dabc1301f2761d776b6
Joe Mordetsky <jmordetsky@zynga.com>
2013-12-03 13:59:17 -0500

    fixed test.

--- game/src/behaviors/RangeBehavior.js
+++ game/src/behaviors/RangeBehavior.js
@@ -111,7 +111,7 @@ RangeBehavior.prototype.doMissile = function(){
 
         var moveTo = cc.MoveTo.create(timeToImpact, targetPos);
         var callback = cc.CallFunc.create(function(){
-           // this.hitLogic();
+            this.hitLogic();
             this.owner.layer.removeChild(this.missile);
             this.firing = false;
             if (this.locked){
@@ -123,8 +123,6 @@ RangeBehavior.prototype.doMissile = function(){
         var seq = cc.Sequence.create(moveTo, callback);
         this.missile.runAction(seq);
 
-    }else{
-        this.hitLogic();
     }
 
 }
